1328bc4a48b28083f77757297836e591721d86b1
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-12-01 12:02:41 +0100

    feat: day 1 - first task

---
+++ day01-1/day01-1.mjs
@@ -0,0 +1,19 @@
+import * as fs from "node:fs";
+
+const inputLines = fs.readFileSync("input.txt", "utf-8").split(/\r?\n/);
+
+let maxValue = 0;
+let currentValue = 0;
+inputLines.forEach((line) => {
+  if (!line.trim()) {
+    console.log("Line value:", currentValue, maxValue);
+
+    maxValue = Math.max(maxValue, currentValue);
+    currentValue = 0;
+    return;
+  }
+
+  currentValue += parseInt(line, 10);
+});
+
+console.log("Max value:", maxValue);
