9e40fcb8154552e38b935522ed7650d34e7cef1b
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-12-02 21:35:51 +0100

    refactor: day 3 prep

--- day02/day02-2.mjs
+++ day02/day02-2.mjs
@@ -1,6 +1,6 @@
 import * as fs from "node:fs";
 
-function readInputLines() {
+function parseLines() {
   return fs.readFileSync("input.txt", "utf-8").split(/\n/);
 }
 
@@ -8,33 +8,44 @@ function sum(array) {
   return array.reduce((total, value) => total + value, 0);
 }
 
-function getPoints(line) {
+function getSymbolPoints(line) {
   switch (line) {
-    case "A X": // Rock + Lose (Scissors)
-      return 0 + 3;
-    case "A Y": // Rock + Draw (Rock)
-      return 3 + 1;
+    case "A Y": // Rock => Draw (Rock)
+    case "B X": // Paper => Lose (Rock)
+    case "C Z": // Scissors => Win (Rock)
+      return 1;
     case "A Z": // Rock + Win (Paper)
-      return 6 + 2;
-    case "B X": // Paper + Lose (Rock)
-      return 0 + 1;
     case "B Y": // Paper + Draw (Paper)
-      return 3 + 2;
-    case "B Z": // Paper + Win (Scissors)
-      return 6 + 3;
     case "C X": // Scissors + Lose (Paper)
-      return 0 + 2;
-    case "C Y": // Scissors + Draw (Scissors)
-      return 3 + 3;
-    case "C Z": // Scissors + Win (Rock)
-      return 6 + 1;
+      return 2;
+    case "A X": // Rock => Lose (Scissors)
+    case "B Z": // Paper => Win (Scissors)
+    case "C Y": // Scissors => Draw (Scissors)
+      return 3;
   }
 }
 
-const lines = readInputLines().map((line) => {
-  console.log("DEBUG lines:", line, getPoints(line));
-  return getPoints(line);
+function getWinPoints(result) {
+  switch (result) {
+    case "Z":
+      return 6;
+    case "Y":
+      return 3;
+    case "X":
+      return 0;
+  }
+}
+
+const points = parseLines().map((line) => {
+  const parts = line.split(/\s+/);
+  console.log(
+    "DEBUG lines:",
+    parts[1],
+    getWinPoints(parts[1]),
+    parts[0],
+    getSymbolPoints(line)
+  );
+  return getWinPoints(parts[1]) + getSymbolPoints(line);
 });
 
-console.log("DEBUG lines:", lines);
-console.log("RESULT:", sum(lines));
+console.log("RESULT:", sum(points));
--- template.js
+++ template.js
@@ -1,19 +1,22 @@
 import * as fs from "node:fs";
 
+function parseLines() {
+  return fs.readFileSync("input.txt", "utf-8").split(/\n/);
+}
+
 function parseLineGroups() {
   const groups = fs.readFileSync("input.txt", "utf-8").split(/\n\s*\n/);
   return groups.map((input) => input.split(/\n/));
 }
+
 function sum(array) {
   return array.reduce((total, value) => total + value, 0);
 }
 
-const totals = parseLineGroups().map((group) => {
-  const values = group.map((line) => parseInt(line, 10));
-  return sum(values);
+const lines = parseLines().map((line) => {
+  const parts = line.split(/\s+/);
+  console.log("DEBUG line", parts);
+  return parts;
 });
 
-totals.sort((a, b) => b - a);
-
-console.log("DEBUG totals:", totals);
-console.log("RESULT:", sum(totals.slice(0, 3)));
+console.log("RESULT:", sum(lines));
