b670445aa1b48bc2a954f5d650d3d213686aa7c0
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-12-04 10:40:01 +0100

    chore: day 5 preparation

--- day04/day04-2.mjs
+++ day04/day04-2.mjs
@@ -1,23 +1,20 @@
 import * as fs from "node:fs";
 import _ from "lodash";
 
-function parseLines() {
+function readLines() {
   return fs.readFileSync("input.txt", "utf-8").split(/\n/);
 }
 
-const pairs = parseLines().map((line) =>
+const pairs = readLines().map((line) =>
   line.split(",").map((pair) => pair.split("-").map((n) => parseInt(n, 10)))
 );
 
 let count = 0;
 pairs.forEach(([first, second]) => {
   const isOverlapping = first[0] <= second[1] && second[0] <= first[1];
+  count += isOverlapping ? 1 : 0;
 
   console.log("DEBUG set", first, second, isOverlapping);
-
-  if (isOverlapping) {
-    count += 1;
-  }
 });
 
 console.log("RESULT:", pairs, count);
