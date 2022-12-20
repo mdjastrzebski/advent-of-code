b18219da8af17ecba0c4d2d8fde8f4d51426a973
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2022-12-20 13:39:03 +0100

    feat: day 20 solution

---
+++ day20/day20-2.mjs
@@ -0,0 +1,125 @@
+import * as fs from "node:fs";
+import _ from "lodash";
+
+function readLines() {
+  return fs.readFileSync("input.txt", "utf-8").split(/\n/);
+}
+
+const decryptKey = 811589153;
+const rawValues = readLines().map((line) => parseInt(line, 10) * decryptKey);
+
+const count = rawValues.length;
+const entries = rawValues.map((value) => ({ value }));
+
+entries.forEach((entry, index) => {
+  entry.previous = entries[(count + index - 1) % count];
+  entry.next = entries[(index + 1) % count];
+});
+
+const zeroEntry = entries.find((v) => v.value === 0);
+
+function printEntry(entry) {
+  console.log(
+    "  *",
+    entry.value,
+    [entry.previous.value, entry.next.previous.value],
+    [entry.previous.next.value, entry.next.value]
+  );
+}
+
+function printValues(label) {
+  console.log(label, ":");
+  let current = entries[0];
+  for (let i = 0; i < count; i += 1) {
+    if (i < 10 || i > count - 10 || current.value === 0) {
+      printEntry(current);
+    }
+    if (i === 10 || i === count - 10) console.log("  ...");
+    current = current.next;
+  }
+}
+
+printValues("Input");
+
+function removeEntry(entry) {
+  const entryPrevious = entry.previous;
+  const entryNext = entry.next;
+  entryPrevious.next = entryNext;
+  entryNext.previous = entryPrevious;
+}
+
+function insertEntryAfter(entry, toInsert) {
+  const nextEntry = entry.next;
+  entry.next = toInsert;
+  toInsert.next = nextEntry;
+
+  nextEntry.previous = toInsert;
+  toInsert.previous = entry;
+}
+
+function insertEntryBefore(entry, toInsert) {
+  const previousEntry = entry.previous;
+  entry.previous = toInsert;
+  toInsert.previous = previousEntry;
+
+  previousEntry.next = toInsert;
+  toInsert.next = entry;
+}
+
+function moveEntry(entry, steps) {
+  if (steps % (count - 1) === 0) {
+    return;
+  }
+
+  let current = entry;
+  removeEntry(entry);
+
+  if (steps > 0) {
+    let xSteps = steps % (count - 1);
+    for (let i = 0; i < xSteps; i += 1) {
+      current = current.next;
+    }
+  } else {
+    let xSteps = -steps % (count - 1);
+    for (let i = 0; i < xSteps; i += 1) {
+      current = current.previous;
+    }
+  }
+
+  if (steps > 0) {
+    insertEntryAfter(current, entry);
+  } else {
+    insertEntryBefore(current, entry);
+  }
+}
+
+function mixEntries(times) {
+  for (let t = 0; t < times; t += 1) {
+    for (let i = 0; i < count; i += 1) {
+      const node = entries[i];
+      moveEntry(node, node.value);
+      // printValues("processing " + node.value);
+    }
+  }
+}
+
+function getEntryAtOffset(entry, offset) {
+  let current = entry;
+  for (let i = 0; i < offset; i += 1) {
+    current = current.next;
+  }
+  return current;
+}
+
+mixEntries(10);
+const node1k = getEntryAtOffset(zeroEntry, 1000);
+const node2k = getEntryAtOffset(zeroEntry, 2000);
+const node3k = getEntryAtOffset(zeroEntry, 3000);
+
+console.log("Node 1k, 2k, 3k:");
+printEntry(node1k);
+printEntry(node2k);
+printEntry(node3k);
+
+const results = [node1k.value, node2k.value, node3k.value];
+console.log("RESULTS", _.sum(results), results);
--- utils/cache.mjs
+++ utils/cache.mjs
@@ -1,29 +1,42 @@
 export class Cache {
   constructor(keySelector, sharedSelector = null) {
+    this._cacheMaps = [];
+
     this.keySelector = keySelector;
     this.sharedSelector = sharedSelector;
-    this.cacheMaps = [];
+    this.size = 0;
+    this.hits = 0;
   }
 
   get(key) {
-    const shard = this.sharedSelector(key);
-    if (!map) {
-      map = new Map();
-      this.cacheMaps[shard] = map;
-    }
-
+    const shard = this._ensureShard(key);
     const keyString = this.keySelector(key);
-    return map.get(keyString);
+
+    this.hits = this.hits + 1;
+    return shard.get(keyString);
   }
 
   set(key, value) {
-    const shard = this.sharedSelector(key);
-    if (!map) {
-      map = new Map();
-      this.cacheMaps[shard] = map;
+    const shard = this._ensureShard(key);
+    const keyString = this.keySelector(key);
+
+    this.size = this.size + 1;
+    return shard.set(keyString, value);
+  }
+
+  get hitRatio() {
+    return (this.hits / this.size).toFixed(2);
+  }
+
+  _ensureShard(key) {
+    const shard = this?.sharedSelector(key) ?? 0;
+    const map = this._cacheMaps[shard];
+    if (map) {
+      return map;
     }
 
-    const keyString = this.keySelector(key);
-    return map.set(keyString, value);
+    const newMap = new Set();
+    this._cacheMaps[shard] = newMap;
+    return newMap;
   }
 }
