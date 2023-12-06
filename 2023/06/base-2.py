9c2ecafaa46e55b3e705ffd78f97dbe05a4fa0d9
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2023-12-06 07:25:00 +0100

    feat: day 6 (own): 19m 59s + 2m 20s

---
+++ 2023/06/base-2.py
@@ -0,0 +1,49 @@
+import re
+
+
+with open("input.txt") as f:
+    input = f.read()
+
+time_str, distance_str = input.split('\n')
+
+
+def parse_line(line):
+    line = line.replace(' ', '')
+    return [int(x) for x in re.findall(r'\d+', line)]
+
+
+times = parse_line(time_str)
+distances = parse_line(distance_str)
+
+print("INPUT", times, distances)
+
+
+def distance_from_time(total_time, charge_time):
+    return (total_time - charge_time) * charge_time
+
+
+def calculate_min_time(time, distance):
+    for i in range(0, time):
+        if distance_from_time(time, i) > distance:
+            return i
+
+    return None
+
+
+def calculate_max_time(time, distance):
+    for i in range(time, 0, -1):
+        if distance_from_time(time, i) > distance:
+            return i
+
+    return None
+
+
+accumulator = 1
+for time, distance in zip(times, distances):
+    min = calculate_min_time(time, distance)
+    max = calculate_max_time(time, distance)
+    accumulator *= max - min + 1
+    print("LINE", time, distance, min, max, max - min,
+          distance_from_time(time, min), distance_from_time(time, max))
+
+print("RESULT", accumulator)
